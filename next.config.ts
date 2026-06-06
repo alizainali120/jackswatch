45e5b0ca2d7f4075d7e9f3de0a54cb46a7849ceb
Ali Zain Ali <alizainali120@gmail.com>
2026-06-06 14:31:44 -0500

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: "/jackswatch",
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "**" },
